6e9db73e37ed2cea6f38c9ad5682044f3ef78e75
Eric Silva <ericdouglas1990@gmail.com>
2021-05-20 20:09:30 -0300

    Desafio 01 de nodejs praticando conceitos de SOLID

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,25 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin.admin) {
+      throw new Error("User not permission!");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -0,0 +1,23 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowUserProfileUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    // Complete aqui
+    const profileUserAlreadyExists = this.usersRepository.findById(user_id);
+
+    if (!profileUserAlreadyExists) {
+      throw new Error("User not found!");
+    }
+
+    return profileUserAlreadyExists;
+  }
+}
+
+export { ShowUserProfileUseCase };
